ec0fe88bdefb7b3f350425d95005c9692bb5e3ec
Burak <burakgomec101@gmail.com>
2021-09-08 12:57:33 +0300

    Add table view's methods for show Fibonacci result

--- Week2/ViewController.swift
+++ Week2/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         configureUI()
     }
     
@@ -31,8 +33,23 @@ class ViewController: UIViewController {
         }) else { return }
         print(resultArray)
         fibonacciResultArray = resultArray
+        self.tableView.reloadData()
     }
 
 
+}
+
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fibonacciResultArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(fibonacciResultArray[indexPath.row])
+        cell.selectionStyle = .none
+        return cell
+    }
 }
 
