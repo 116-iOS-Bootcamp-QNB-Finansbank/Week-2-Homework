56e40c40a45398c23827b47a856f40719417afbc
Burak <burakgomec101@gmail.com>
2021-09-08 12:37:29 +0300

    Add configure method for bar button items

--- Week2/ViewController.swift
+++ Week2/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    
+    private func configureUI(){
+        let rightBarButtonItem = UIBarButtonItem(title: "Calculate Fibonacci", style: UIBarButtonItem.Style.plain, target: self, action: #selector(showFibonacciResults))
+        rightBarButtonItem.tintColor = .red
+        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    
+    @objc func showFibonacciResults(){
+        
     }
 
 
